68e38773002f917e64a9365049b1e93793e00ae4
Jaxon Porter <10637687@uvu.edu>
2021-06-01 19:41:32 -0600

    Initial commit

---
+++ ClinicalPassport/Migrations/20210602012614_initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClinicalPassport.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentID", "Address", "DOB", "Name" },
+                values: new object[] { 13512, "123 Nowhere", new DateTime(2021, 6, 1, 0, 0, 0, 0, DateTimeKind.Local), "Dwayne 'The Rock' Johnson" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ ClinicalPassport/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicalPassport.Models
+{
+	public class Student
+	{
+		public int StudentID { get; set; }
+		public string Name { get; set; }
+		public DateTime DOB { get; set; }
+		public string Address { get; set; }
+	}
+}
