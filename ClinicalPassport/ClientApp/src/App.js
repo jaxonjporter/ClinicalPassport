29b31c9c0fe91e401d7ddc3b142988d13f98bddd
Jaxon Porter <10637687@uvu.edu>
2021-06-19 15:04:44 -0600

    Added semantic and basic dashboard functionality

--- ClinicalPassport/ClientApp/src/App.js
+++ ClinicalPassport/ClientApp/src/App.js
@@ -4,19 +4,23 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react'
+
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
+export default function App (props) {
+  const displayName = App.name;
 
-  render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Layout>
+            <Container>
+                <Route exact path='/' component={Home} />
+                <Route path='/counter' component={Counter} />
+                <Route path='/fetch-data' component={FetchData} />
+            </Container >
       </Layout>
     );
   }
-}
+
