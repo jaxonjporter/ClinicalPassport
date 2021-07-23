cd57e25a8b292727de58e6d58dd4d6648efed0f5
matthewmgraf <79622625+matthewmgraf@users.noreply.github.com>
2021-07-22 21:17:29 -0600

    Added controllers.

---
+++ ClinicalPassport/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using ClinicalPassport.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicalPassport.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private ClinicalPassportContext _context { get; set; }
+
+        public DashboardController(ClinicalPassportContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Json("Hi");
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(int userId)
+        {
+            return Json(userId);
+        }
+    }
+}
