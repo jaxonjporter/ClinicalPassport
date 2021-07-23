34541934ab317f90bdc9a0842fc05787b3552459
matthewmgraf <79622625+matthewmgraf@users.noreply.github.com>
2021-07-22 21:14:05 -0600

    Added controllers.

---
+++ ClinicalPassport/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ClinicalPassport.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicalPassport.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ClinicalPassportContext _context { get; set; }
+
+        public HomeController(ClinicalPassportContext ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var data = _context.Users;
+            return Json(data);
+        }
+    }
+}
